8c53a927ced655eddf8390e99e1e8d6a36e1ecd3
Srinivasa Kumar <srinivasakumarchintapatla@gmail.com>
2025-11-21 13:40:14 +0530

    13.40

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "myapp"
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('item/', views.item),
-    path('<int:id>/', views.detail), 
+    path('<int:id>/', views.detail, name='detail'), 
 ]
