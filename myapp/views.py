93c9700fa98c9b56554b07a6f9e05d188a2cf305
Srinivasa Kumar <srinivasakumarchintapatla@gmail.com>
2025-11-21 12:13:47 +0530

    12-13

--- myapp/views.py
+++ myapp/views.py
@@ -4,11 +4,24 @@ from .models import Item
 
 # Create your views here.
 def index(request):
+    # Geting items from database
     item_list = Item.objects.all()
-
-    context = {'item_list': item_list}
+    # Creating context
+    context = {
+        'item_list': item_list
+        }
+    # Passing the context object to the render method along with the template
     return render(request, "myapp/index.html", context)
-    
+
+def detail(request, id):
+    item = Item.objects.get(id=id)
+    context = {
+        'item': item
+    }
+    return render(request, "myapp/detail.html", context)
+
+
+
 
 def item(request):
     return HttpResponse("<h1>This is the item view.</h1>")
