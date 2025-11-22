8803abc821686344d979a52682caf2511db7e70e
Srinivasa Kumar <srinivasakumarchintapatla@gmail.com>
2025-11-22 22:15:57 +0530

    22.15

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+
 
 urlpatterns = [
     # /users/ -> signup page
-    path('', views.signup, name='signup'),
-
-    # Legacy/alternate URL: /users/register/
+    # path('', views.signup, name='signup'),
     path('register/', views.signup, name='register'),
+   path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    # path('logout/',auth_views.LogoutView.as_view(),name='logout'),
 ]
+
 
 
 
